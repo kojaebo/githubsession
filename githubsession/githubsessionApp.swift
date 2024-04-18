24f0dbd81e1e0deb46dbfa5803c28d2679ae682d
kojaebo <kojaebo@gmail.com>
2024-04-18 20:12:28 +0900

    Initial Commit

---
+++ githubsession/githubsessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubsessionApp.swift
+//  githubsession
+//
+//  Created by 고재보 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct githubsessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
